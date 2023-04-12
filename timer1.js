aa25fcf4fce37f56cc6d757f588318ab00cc00bf
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-11 18:14:18 -0600

    even more refactoring completed

--- timer1.js
+++ timer1.js
@@ -2,8 +2,8 @@ const args = process.argv.slice(2);
 
 for (let arg of args) {
 
-  if (arg.length === 0 || arg < 0 || isNaN(arg) === true) {
-    return;}
+  if (arg.length === 0 || arg < 0 || isNaN(arg)) {
+    continue;}
 
   setTimeout(() => {
     process.stdout.write('\x07');
